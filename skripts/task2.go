212229485b83e1d40811c374cfbb35be1633d12f
muhammedali <muhammedali2000@mail.ru>
2025-09-25 18:16:53 +0500

    second task

---
+++ skripts/task2.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Ali"
+	var age int = 20
+	var isstudent bool = true
+	fmt.Println("Hello,", name, "you are", age, "years old. And is he student?", isstudent)
+}
